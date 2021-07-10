efa7409a44684fee1e11657668a65c1f8f3daa3f
AndrusovN <andrusov.n@gmail.com>
2021-07-10 20:31:48 +0300

    small changes

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from app import app
 from ManageBackApi.update_all import update_all
 from ManageBackApi.DB_subjects_STS import update_subjects_tasks_marks
 
-# update_all()
-app.run(port=5000)
+
+if __name__ == '__main__':
+    app.run(port=5000)
