2fc2847d0c7957632567be9511f22ae1837d944f
Setka1324 <mstemchenko@yandex.ru>
2021-08-20 13:18:25 +0300

    Xcel

--- app/ApiHandlers/ExcelExport.py
+++ app/ApiHandlers/ExcelExport.py
@@ -1,4 +1,5 @@
 import datetime
+import xlsxwriter
 
 from flask_restful import Resource, reqparse
 from app.ApiHandlers.JWTVerification import check_access_token
@@ -7,7 +8,93 @@ from app.Database import Students, Marks
 
 
 def generate_excel(marks, filename):
-    pass
+    
+    workbook = xlsxwriter.Workbook(filename + ".xlsx")
+    #Создаю файл xlsx
+    
+    #Создание таблицы Summative
+    def table_sum(marks):
+        worksheet_sum = workbook.add_worksheet('Summative')
+        #Создаю лист с таблицей
+
+        line_index = {'A': {'page' : 1, 'line' : 0}, 'B': {'page' : 1, 'line' : 1}, 'C': {'page' : 1, 'line' : 2}, 'D': {'page' : 1, 'line' : 3}, '0': {'page' : 1, 'line' : 4}}
+        #Параметры для записи оценок
+
+        worksheet_sum.write(line_index['A']['line'],0,"A")
+        worksheet_sum.write(line_index['B']['line'],0,"B")
+        worksheet_sum.write(line_index['C']['line'],0,"C")
+        worksheet_sum.write(line_index['D']['line'],0,"D")
+        worksheet_sum.write(line_index['0']['line'],0,"None")
+        #Рисую критерии
+
+        for mark in marks:
+
+            if mark['type'] == 'summative':
+            #Проверяю тип оценки
+                
+                worksheet_sum.write_number(line_index[mark['criteria']]['line'],line_index[mark['criteria']]['page'],int(mark['mark']))
+                #Записываю оценку
+
+                line_index[mark['criteria']]['page'] += 1
+                #Меняю ячейку для следующей записи
+
+
+    #Создание таблицы Formative
+    def table_form(marks):
+        worksheet_form = workbook.add_worksheet('Formative')
+        #Создаю лист с таблицей
+
+        line_index = {'A': {'page' : 1, 'line' : 0}, 'B': {'page' : 1, 'line' : 1}, 'C': {'page' : 1, 'line' : 2}, 'D': {'page' : 1, 'line' : 3}, '0': {'page' : 1, 'line' : 4}}
+        #Параметры для записи оценок
+
+
+        worksheet_form.write(line_index['A']['line'],0,"A")
+        worksheet_form.write(line_index['B']['line'],0,"B")
+        worksheet_form.write(line_index['C']['line'],0,"C")
+        worksheet_form.write(line_index['D']['line'],0,"D")
+        worksheet_form.write(line_index['0']['line'],0,"None")
+        #Рисую критерии
+
+        for mark in marks:
+
+            if mark['type'] == 'formative':
+            #Проверяю тип оценки
+                worksheet_form.write_number(line_index[mark['criteria']]['line'],line_index[mark['criteria']]['page'],int(mark['mark']))
+                #Записываю оценку
+
+                line_index[mark['criteria']]['page'] += 1
+                #Меняю ячейку для следующей записи
+
+    #Создание таблицы Mixed
+    def table_mix(marks):
+        worksheet_mix = workbook.add_worksheet('Mixed')
+        #Создаю лист с таблицей
+
+        line_index = {'A': {'page' : 1, 'line' : 0}, 'B': {'page' : 1, 'line' : 1}, 'C': {'page' : 1, 'line' : 2}, 'D': {'page' : 1, 'line' : 3}, '0': {'page' : 1, 'line' : 4}}
+        #Параметры для записи оценок
+
+
+        worksheet_mix.write(line_index['A']['line'],0,"A")
+        worksheet_mix.write(line_index['B']['line'],0,"B")
+        worksheet_mix.write(line_index['C']['line'],0,"C")
+        worksheet_mix.write(line_index['D']['line'],0,"D")
+        worksheet_mix.write(line_index['0']['line'],0,"None")
+        #Рисую критерии
+
+        for mark in marks:
+            worksheet_mix.write_number(line_index[mark['criteria']]['line'],line_index[mark['criteria']]['page'],int(mark['mark']))
+            #Записываю оценку
+
+            line_index[mark['criteria']]['page'] += 1
+            #Меняю ячейку для следующей записи
+
+
+    
+    table_sum(marks)
+    table_form(marks)
+    table_mix(marks)
+    workbook.close()
+    
 
 
 # Получение всех классов, которые есть в системе
